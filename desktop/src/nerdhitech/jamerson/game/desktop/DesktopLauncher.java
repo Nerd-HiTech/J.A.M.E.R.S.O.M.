6e5f2183dd2e569ac62d5172ba7b1791c62d1d85
Victor Perin <victor_perin@nerdhitech.com.br>
2014-07-05 10:24:51 -0300

    libGDX generated

---
+++ desktop/src/nerdhitech/jamerson/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package nerdhitech.jamerson.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import nerdhitech.jamerson.game.jamerson;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new jamerson(), config);
+	}
+}
---
+++ html/src/nerdhitech/jamerson/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package nerdhitech.jamerson.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import nerdhitech.jamerson.game.jamerson;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new jamerson();
+        }
+}
